290b15f535baf7788a03973b14b4d8318042f68c
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 06:43:54 +0200

    added first basic styling

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-  return <h2>Expense item CommitTest 4!</h2>
+  return (
+    <div className="expense-item">
+      <div>15.06.2022</div>
+      <div className="expense-item__description">
+        <h2>Car ins</h2>
+        <div className="expense-item__price">€235,54</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
