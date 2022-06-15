c54a3bab31e41157cfe5a0e7c68cdf597c0e5be7
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 14:05:58 +0200

    first use of useState

--- react-complete-guide/src/components/expenses/ExpenseItem.js
+++ react-complete-guide/src/components/expenses/ExpenseItem.js
@@ -1,18 +1,28 @@
 import './ExpenseItem.css'
+import {useState} from 'react';
 
 import Card from '../ui/Card'
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); // useState-ReactHook
+  
+  
+  const clickHandler = () => {
+    setTitle('Updated :)');
+    console.log(title); // Title ist hier direkt danach noch das alte value!
+  };
+  
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
