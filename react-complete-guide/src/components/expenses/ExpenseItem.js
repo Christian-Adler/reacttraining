70959d12d53b175a213331bee3e7723a3876ef33
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 20:55:26 +0200

    ExpenseItem back to stateles component

--- react-complete-guide/src/components/expenses/ExpenseItem.js
+++ react-complete-guide/src/components/expenses/ExpenseItem.js
@@ -1,30 +1,18 @@
-import './ExpenseItem.css'
-import {useState} from 'react';
-
-import Card from '../ui/Card'
+import "./ExpenseItem.css";
+import Card from "../ui/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title); // useState-ReactHook
-  
-  
-  const clickHandler = () => {
-    setTitle('Updated :)');
-    console.log(title); // Title ist hier direkt danach noch das alte value!
-  };
-  
-  
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
-      
+      <ExpenseDate date={props.date} />
+
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
