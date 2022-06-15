604d958ee8e555cd3d0a6b436881a1a6d72678ea
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 22:57:03 +0200

    conditional returns - more readable

--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -1,9 +1,9 @@
-import "./Expenses.css";
-
 import { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import Card from "../ui/Card";
 
 const Expenses = (props) => {
@@ -15,17 +15,6 @@ const Expenses = (props) => {
     (item) => `${item.date.getFullYear()}` === filteredYear
   );
 
-  let expensesContent = <p>No expenses found</p>;
-  if (filteredItems.length > 0)
-    expensesContent = filteredItems.map((item) => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      />
-    ));
-
   return (
     <div>
       <Card className="expenses">
@@ -33,7 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredItems} />
       </Card>
     </div>
   );
