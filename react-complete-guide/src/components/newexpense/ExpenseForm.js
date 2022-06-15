bf3ea7decb9960991c9414f38661de43b895bbe4
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 15:20:24 +0200

    using multiple states

---
+++ react-complete-guide/src/components/newexpense/ExpenseForm.js
@@ -0,0 +1,39 @@
+import './ExpenseForm.css'
+import {useState} from 'react';
+
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  const onTitleChange = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const onAmountChange = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const onDateChange = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  
+  return <form>
+    <div className="new-expense__controls">
+      <div className="new-expense__control">
+        <label>Titel</label>
+        <input type="text" onChange={onTitleChange} value={enteredTitle}/>
+      </div>
+      <div className="new-expense__control">
+        <label>Amount</label>
+        <input type="number" min="0.01" step="0.01" onChange={onDateChange}/>
+      </div>
+      <div className="new-expense__control">
+        <label>Date</label>
+        <input type="date" min="2019-01-01" max="2022-12-31" onChange={onAmountChange}/>
+      </div>
+    </div>
+    <div className="new-expense__actions">
+      <button type="submit">Add Expense</button>
+    </div>
+  </form>;
+}
+export default ExpenseForm;
---
+++ react-complete-guide/src/components/newexpense/NewExpense.js
@@ -0,0 +1,10 @@
+import './NewExpense.css'
+import ExpenseForm from "./ExpenseForm";
+// import {useState} from 'react';
+
+const NewExpense = (props) => {
+  return <div className="new-expense">
+    <ExpenseForm/>
+  </div>
+}
+export default NewExpense;
