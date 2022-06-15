5ca1ffd04967cb2a61c0d9d812b96b292997bf35
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 11:01:29 +0200

    added Card (Container) with special props.children

--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -1,14 +1,15 @@
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card'
 
 function Expenses(props) {
   const { items } = props;
-  return <div className="expenses">
+  return <Card className="expenses">
     <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date}/>
     <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date}/>
     <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date}/>
     <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date}/>
-  </div>;
+  </Card>;
 }
 
 export default Expenses;
