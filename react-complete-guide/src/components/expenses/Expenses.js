2512bc388e2e0857c05bb012c34e8eab9836f306
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-15 20:51:43 +0200

    added ExpenseFilter with 2 way binding

--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -1,15 +1,48 @@
-import './Expenses.css';
+import "./Expenses.css";
+
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
-import Card from '../ui/Card'
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../ui/Card";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
   const { items } = props;
-  return <Card className="expenses">
-    <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date}/>
-    <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date}/>
-    <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date}/>
-    <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date}/>
-  </Card>;
-}
+
+  const filterChangeHandler = (year) => setFilteredYear(year);
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
