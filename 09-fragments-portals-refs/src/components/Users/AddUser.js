330e0f4368004bb41f2b0d525c2794bda41d2bfb
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-17 16:07:15 +0200

    added Wrapper to prevent unnecessary div

--- 09-fragments-portals-refs/src/components/Users/AddUser.js
+++ 09-fragments-portals-refs/src/components/Users/AddUser.js
@@ -44,7 +44,7 @@ const AddUser = (props) => {
   };
   
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
---
+++ 09-fragments-portals-refs/src/components/helpers/Wrapper.js
@@ -0,0 +1,5 @@
+// Wrapper as single return value without a <div> needed = React.Fragment
+const Wrapper = (props) => {
+  return props.children;
+}
+export default Wrapper;
