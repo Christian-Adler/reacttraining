397a317cbbedbacdf0da363a1ed5201ee73d49e8
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-17 16:11:55 +0200

    use React.Fragment as Wrapper for less divs

--- 09-fragments-portals-refs/src/App.js
+++ 09-fragments-portals-refs/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, {Fragment, useState} from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
   const [usersList, setUsersList] = useState([]);
-
+  
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
       return [
@@ -14,12 +14,24 @@ function App() {
       ];
     });
   };
-
+  
+  // return (
+  //   <React.Fragment>
+  //     <AddUser onAddUser={addUserHandler}/>
+  //     <UsersList users={usersList}/>
+  //   </React.Fragment>
+  // );
+  // return (
+  //   <>  // Diesen Syntax muss das Projekt verstehen
+  //     <AddUser onAddUser={addUserHandler}/>
+  //     <UsersList users={usersList}/>
+  //   </>
+  // );
   return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </div>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </Fragment>
   );
 }
 
