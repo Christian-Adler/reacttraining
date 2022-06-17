85652c26c5d650574185e130527d0dada41a2934
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-17 20:51:46 +0200

    added side effect read once from local storage

--- 10-effects-reducers-context/src/App.js
+++ 10-effects-reducers-context/src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // runs after every component evaluation
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []); // Keine Dependencies -> die interne Funktion wird nur einmalig ausgefuehrt - da sich die Dependencies nicht veraendern.
+
   const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
