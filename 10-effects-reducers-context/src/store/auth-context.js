7b7fd3d00713b9d2e514ebb49ffcbaa1f5cf839c
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-18 14:59:02 +0200

    Default context always complete with dummys for IDE auto completion

--- 10-effects-reducers-context/src/store/auth-context.js
+++ 10-effects-reducers-context/src/store/auth-context.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}, // Dummy - damit die IDE das in Autocompletion anzeigt.
 });
 
 export default AuthContext;
