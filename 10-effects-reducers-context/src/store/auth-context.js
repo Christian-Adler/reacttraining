a63341fa3830c51ea1838f7b312a2ee44f0f2fa3
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-18 15:43:26 +0200

    one central auth context management

--- 10-effects-reducers-context/src/store/auth-context.js
+++ 10-effects-reducers-context/src/store/auth-context.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {}, // Dummy - damit die IDE das in Autocompletion anzeigt.
+  onLogin: (email, password) => {}, // Dummy - damit die IDE das in Autocompletion anzeigt.
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // runs after every component evaluation
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []); // Keine Dependencies -> die interne Funktion wird nur einmalig ausgefuehrt - da sich die Dependencies nicht veraendern.
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
