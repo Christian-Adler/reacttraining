a46c62e5f19f6fd297ec4008a1a0e2c64d653ea4
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-18 07:10:48 +0200

    use useReducer (komplex state)

--- 10-effects-reducers-context/src/components/Login/Login.js
+++ 10-effects-reducers-context/src/components/Login/Login.js
@@ -1,63 +1,96 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return {
+      value: action.val,
+      isValid: action.val.includes("@"),
+    };
+  } else if (action.type === "INPUT_BLUR") {
+    return {
+      value: state.value,
+      isValid: state.value.includes("@"),
+    };
+  }
+  return { value: "", isValid: false };
+};
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return {
+      value: action.val,
+      isValid: action.val.trim().length > 6,
+    };
+  } else if (action.type === "INPUT_BLUR") {
+    return {
+      value: state.value,
+      isValid: state.value.trim().length > 6,
+    };
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(
-    () => {
-      console.log("EFFECT RUNNING");
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
 
-      // Wird asugefueht bevor useEffect ERNEUT ausgefuehrt wird
-      // oder wenn die Komponente aus dem DOM entfernt (aufgeraeumt) wird.
-      return () => {
-        console.log("EFFECT CLEANUP");
-      };
-    }
-    //      >> Nach jedem State-Update / RenderCycle
-    // , []   >> Nach erstmaligem Rendern - Abhaengigkeiten aendern sich nie
-    // , [enteredPassword]  >> nachdem sich das Passwort geandert hat.
-  );
+  // useEffect(
+  //   () => {
+  //     console.log("EFFECT RUNNING");
+  //
+  //     // Wird asugefueht bevor useEffect ERNEUT ausgefuehrt wird
+  //     // oder wenn die Komponente aus dem DOM entfernt (aufgeraeumt) wird.
+  //     return () => {
+  //       console.log("EFFECT CLEANUP");
+  //     };
+  //   }
+  //   //      >> Nach jedem State-Update / RenderCycle
+  //   // , []   >> Nach erstmaligem Rendern - Abhaengigkeiten aendern sich nie
+  //   // , [enteredPassword]  >> nachdem sich das Passwort geandert hat.
+  // );
 
   useEffect(() => {
     const identifier = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
+      console.log("SET FORM IS VALID");
+      setFormIsValid(emailState.isValid && passwordState.isValid);
     }, 500);
 
     // Cleanup function - nicht jeder Tastenanschlag wird geprueft (z.B. zum Server geschickt)
     return () => {
       clearTimeout(identifier);
     };
-  }, [enteredEmail, enteredPassword]);
+  }, [emailState.isValid, passwordState.isValid]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value.trim() });
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({ type: "INPUT_BLUR" }); // Kein value hier, da sich beim blur (focus lost) der Wert nicht aendert.
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -65,28 +98,28 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
