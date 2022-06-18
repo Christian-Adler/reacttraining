02361fed81729df5100f38093dc38e26ab0f61da
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-18 21:27:31 +0200

    used "forwar Refs" to control focus (or eg. scrolling or something like that - use rarely or never ;)

--- 10-effects-reducers-context/src/components/UI/Input/Input.js
+++ 10-effects-reducers-context/src/components/UI/Input/Input.js
@@ -1,8 +1,23 @@
 // import {useState} from "react";
 import classes from "./Input.module.css";
-import React from "react";
+import React, { useImperativeHandle, useRef } from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  /**
+   * focus
+   */
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
   return (
     <div
       className={`${classes.control} ${
@@ -11,6 +26,7 @@ const Input = (props) => {
     >
       <label htmlFor="{props.id}">{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -19,6 +35,6 @@ const Input = (props) => {
       />
     </div>
   );
-};
+});
 
 export default Input;
