4ee7a850cb990e90ad24861218e3365cff4f234a
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-18 05:45:36 +0200

    use useEffect cleanup function

--- 10-effects-reducers-context/src/components/Login/Login.js
+++ 10-effects-reducers-context/src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // Cleanup function - nicht jeder Tastenanschlag wird geprueft (z.B. zum Server geschickt)
+    return () => {
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
