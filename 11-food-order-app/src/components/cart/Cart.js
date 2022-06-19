fe4e8e4143936cdcf83a8092fcca386caa7923ae
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-19 14:15:01 +0200

    show Cart as Modal

--- 11-food-order-app/src/components/cart/Cart.js
+++ 11-food-order-app/src/components/cart/Cart.js
@@ -1,18 +1,19 @@
 // import {useState} from "react";
 
 import classes from "./Cart.module.css";
+import Modal from "../ui/Modal";
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amout: 2, price: 12.99 }].map((item) => (
-        <li>item.name</li>
+      {[{ id: "c1", name: "Sushi", amout: 2, price: 12.99 }].map((item) => (
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -22,7 +23,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
