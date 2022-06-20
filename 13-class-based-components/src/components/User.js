cbef2dca00bad8d0febb6c228a6dfafd1cb4d476
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-20 21:25:10 +0200

    users component as class

--- 13-class-based-components/src/components/User.js
+++ 13-class-based-components/src/components/User.js
@@ -1,7 +1,21 @@
 import classes from './User.module.css';
+import {Component} from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  // constructor() {
+  // }
+  
+  /**
+   * specific method expected from react
+   */
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// functional
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
