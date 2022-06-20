a2ffe09a37d5254bc979dd4ca6360ef87f070c50
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-20 12:47:32 +0200

    added small side effect bumping the icon on change

--- 11-food-order-app/src/components/layout/HeaderCartButton.js
+++ 11-food-order-app/src/components/layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
