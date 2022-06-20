3a8f8a55b50a09168687847808a0bd4b99a9effa
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-20 16:51:01 +0200

    Toggle rerenders only parts of the real DOM

--- 12-optimization/src/App.js
+++ 12-optimization/src/App.js
@@ -1,7 +1,22 @@
+import React, {useState} from 'react';
+
+import './App.css';
+import Button from "./components/UI/Button/Button";
+
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  
+  console.log('App running');
+  
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevState) => !prevState);
+  };
+  
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>ShowParagraph</Button>
     </div>
   );
 }
