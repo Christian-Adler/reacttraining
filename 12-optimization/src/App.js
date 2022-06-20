a1f306c7d7aae0b32aa9b662fe6d555b5aa3b58b
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-20 19:44:35 +0200

    useCallback with dependencies zu recreate the function (because otherwise never because in closure)

--- 12-optimization/src/App.js
+++ 12-optimization/src/App.js
@@ -6,6 +6,7 @@ import DemoOutput from "./components/demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
   
   console.log('App running');
   
@@ -13,8 +14,14 @@ function App() {
   // => memo() comparisson then works -> same (not equal) function
   
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevState) => !prevState);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevState) => !prevState);
+    }
+  }, [allowToggle]);
+  
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
   
   // Obwohl show={false} wird auch die Kindkomponente jedes Mal reevaluiert, wenn Parent.
   // Nur die State-Aenderung ist fuer Re-Eval ausschlaggebend. Nicht die Props die nach unten gegeben werden.
@@ -22,7 +29,8 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow toggeling</Button>
       <Button onClick={toggleParagraphHandler}>ShowParagraph</Button>
     </div>
   );
