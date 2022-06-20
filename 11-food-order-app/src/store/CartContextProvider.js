ac36025a17b793b95a136e3a4bccd02d87d59960
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-20 12:35:56 +0200

    added remove cart item to complex reducer

--- 11-food-order-app/src/store/CartContextProvider.js
+++ 11-food-order-app/src/store/CartContextProvider.js
@@ -13,7 +13,7 @@ const cartReducer = (state, action) => {
       state.totalAmount + action.item.price * action.item.amount;
 
     const existingCartItemIndex = state.items.findIndex(
-      (item) => (item.id = action.item.id)
+      (item) => item.id === action.item.id
     );
 
     const existingCartItem = state.items[existingCartItemIndex];
@@ -34,7 +34,32 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
-  } else {
+  } else if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+    // last item? > remove from list (new list without item)
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    // return new state object
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
