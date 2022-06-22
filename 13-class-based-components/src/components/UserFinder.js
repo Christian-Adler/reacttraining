192151b88b4ed0e0b79a5cf09037abff674b8d62
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-22 21:21:09 +0200

    class based - ErrorBoundary - not possible with functional components

--- 13-class-based-components/src/components/UserFinder.js
+++ 13-class-based-components/src/components/UserFinder.js
@@ -2,6 +2,7 @@ import {Component, Fragment} from 'react';
 import Users from './Users';
 import classes from './UserFinder.module.css'
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   // only one static context possible
@@ -48,7 +49,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type='search' onChange={this.searchChangeHandler.bind(this)}/>
         </div>
-        <Users users={this.state.filteredUsers}/>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers}/>
+        </ErrorBoundary>
       </Fragment>
     );
   }
