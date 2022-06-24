e6727381ab07d437762da98a99ca583b10ecd99d
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-24 13:57:33 +0200

    custom hook with parameter - reusable

--- 15-custom-react-hooks/src/hooks/use-counter.js
+++ 15-custom-react-hooks/src/hooks/use-counter.js
@@ -4,16 +4,16 @@ import { useEffect, useState } from "react";
  * Name has to start with use!!
  * @returns {number}
  */
-const useCounter = () => {
+const useCounter = (step = 1) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + step);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [step]);
 
   return counter;
 };
