f9d39f5ddadb27ef21ac50f89212a047b94b13d4
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-06-30 21:15:38 +0200

    redux demo with actions

--- 18-redux-demo/redux-demo.js
+++ 18-redux-demo/redux-demo.js
@@ -8,9 +8,16 @@ const defaultState = {
  * pure function - no side effects!
  */
 const counterReducer = (state = defaultState, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment")
+    return {
+      counter: state.counter + 1,
+    };
+  else if (action.type === "decrement")
+    return {
+      counter: state.counter - 1,
+    };
+
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -26,3 +33,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
