6b45be0114753ad961f4d23673febfe266beff2d
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-06 06:49:11 +0200

    added Links - so far not exact routing

--- 20-react-router/src/pages/Products.js
+++ 20-react-router/src/pages/Products.js
@@ -2,13 +2,18 @@
 // import "./Products.css";
 // import classes from "./Products.module.css";
 
+import {Link} from "react-router-dom";
+
 const Products = (props) => {
   return (<section>
       <h1>The products page</h1>
       <ul>
-        <li>Product 1 - book</li>
-        <li>Product 2 - carpet</li>
-        <li>Product 3 - online course</li>
+        <li>
+          <Link to="/products/p1">Product 1 - book</Link></li>
+        <li>
+          <Link to="/products/p2">Product 2 - carpet</Link></li>
+        <li>
+          <Link to="/products/p3">Product 3 - online course</Link></li>
       </ul>
     </section>
   );
