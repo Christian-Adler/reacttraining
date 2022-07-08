758d1e8167b54fe853348b4fa00be02e412cd5d5
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-08 08:05:51 +0200

    added more lazy loading

--- 21-deployment/src/App.js
+++ 21-deployment/src/App.js
@@ -1,13 +1,15 @@
 import React, {Suspense} from "react";
 import {Redirect, Route, Switch} from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
+// import QuoteDetail from "./pages/QuoteDetail";
 //import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
 
 const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
