fab5de9aff64197e22b89a97288eb2e694f59d1e
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-08 05:56:12 +0200

    Switch to Routes and child to element-property

--- 20-react-router-v5-to-v6/src/App.js
+++ 20-react-router-v5-to-v6/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -8,19 +8,13 @@ import MainHeader from './components/MainHeader';
 function App() {
   return (
     <div>
-      <MainHeader />
+      <MainHeader/>
       <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/welcome" element={<Welcome/>}/>
+          <Route path="/products" element={<Products/>} exact/>
+          <Route path="/products/:productId" element={<ProductDetail/>}/>
+        </Routes>
       </main>
     </div>
   );
