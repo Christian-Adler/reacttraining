fd41660ab19c6ee4506bfa1bc00eb4df634ee283
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-08 06:39:00 +0200

    Links (and path) are as well relative

--- 20-react-router-v5-to-v6-2nd/src/pages/Welcome.js
+++ 20-react-router-v5-to-v6-2nd/src/pages/Welcome.js
@@ -1,11 +1,14 @@
-import {Route, Routes} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom';
+import {Fragment} from "react";
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
+      <Link to="new-user">New user</Link>
       <Routes>
-        <Route path="new-user" element={<p>Welcome, new user!</p>}/>
+        <Route path="new-user" element={<Fragment><p>Welcome, new user!</p>
+          <Link to="../">Close New user</Link></Fragment>}/>
       </Routes>
     </section>
   );
