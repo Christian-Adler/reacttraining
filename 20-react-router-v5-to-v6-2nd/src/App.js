7267f83599126d3cf574b25e8ff1a8b8cb9ba144
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-08 06:51:06 +0200

    nested routes in App with Outlet target

--- 20-react-router-v5-to-v6-2nd/src/App.js
+++ 20-react-router-v5-to-v6-2nd/src/App.js
@@ -1,9 +1,10 @@
-import {Navigate, Route, Routes} from 'react-router-dom';
+import {Link, Navigate, Route, Routes} from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
+import {Fragment} from "react";
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Navigate replace to='/welcome'/>}/>
-          <Route path='/welcome/*' element={<Welcome/>}/>
+          <Route path='/welcome/*' element={<Welcome/>}>
+            <Route path="new-user" element={<Fragment><p>Welcome, new user!</p>
+              <Link to="../">Close New user</Link></Fragment>}/>
+          </Route>
           <Route path='/products' element={<Products/>}/>
           <Route path='/products/:productId' element={<ProductDetail/>}/>
         </Routes>
--- 20-react-router-v5-to-v6-2nd/src/pages/Welcome.js
+++ 20-react-router-v5-to-v6-2nd/src/pages/Welcome.js
@@ -1,15 +1,11 @@
-import {Link, Route, Routes} from 'react-router-dom';
-import {Fragment} from "react";
+import {Link, Outlet} from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
       <Link to="new-user">New user</Link>
-      <Routes>
-        <Route path="new-user" element={<Fragment><p>Welcome, new user!</p>
-          <Link to="../">Close New user</Link></Fragment>}/>
-      </Routes>
+      <Outlet/>
     </section>
   );
 };
