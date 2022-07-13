eb4b35e44b30d3bec03d68bdfd725b028532198d
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-13 21:49:00 +0200

    used Link to navigate through pages

--- 23-nextjs-course/pages/news/[newsId].js
+++ 23-nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,8 @@
 // .../news/anything
 
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Link from "next/link";
 
 function DetailPage() {
   const router = useRouter();
@@ -9,7 +11,12 @@ function DetailPage() {
 
   // send a request to the backend API to fetch news item with newsId
 
-  return <h1>Something:{newsId}</h1>;
+  return (
+    <Fragment>
+      <h1>Something:{newsId}</h1>
+      <Link href="/news">Back to news</Link>
+    </Fragment>
+  );
 }
 
 export default DetailPage;
