2f82d6a6d2ef4e438bbc009c28c79acc01678a3d
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-20 08:59:17 +0200

    first meetup detail page (hardcoded)

---
+++ 23-nextjs-biggerproject/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import {Fragment} from "react";
+
+const MeetupDetails = () => {
+  return <Fragment>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/HNL_Wiki_Wiki_Bus.jpg/1280px-HNL_Wiki_Wiki_Bus.jpg" alt="a first meetup"/>
+    <h1>A First meetup</h1>
+    <address>Sieosdofm</address>
+    <p>Some Desc</p>
+  </Fragment>
+};
+
+export default MeetupDetails;
