2b007bad505c77d2c27e2225b59ce97996bfecf0
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-23 17:59:55 +0200

    added test suite to cover all test for one group

--- 26-testing/src/components/Greeting.test.js
+++ 26-testing/src/components/Greeting.test.js
@@ -1,14 +1,19 @@
 import {render, screen} from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test('Greeting: renders Hello World', () => {
-  // Arrange
-  render(<Greeting/>);
+describe('Test suite: Greeting component', () => {
   
-  // Act
-  // .. nothing - no functions calls here
+  test('Greeting: renders Hello World', () => {
+    // Arrange
+    render(<Greeting/>);
+    
+    // Act
+    // .. nothing - no functions calls here
+    
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World', { exact: false }); // false, weil ! noch im DOM steht
+    expect(helloWorldElement).toBeInTheDocument();
+  });
   
-  // Assert
-  const helloWorldElement = screen.getByText('Hello World', { exact: false }); // false, weil ! noch im DOM steht
-  expect(helloWorldElement).toBeInTheDocument();
 });
+
