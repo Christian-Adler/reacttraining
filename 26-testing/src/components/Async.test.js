dd7a6c206030f12e9f2d3831578fecd2d67c28f5
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-23 18:36:43 +0200

    added mock for fetch

--- 26-testing/src/components/Async.test.js
+++ 26-testing/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    
+    // override build in fetch function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({ json: async () => [{ id: 'p1', title: 'First Post' }] });
     render(<Async/>);
     
     // get ruft direkt auf - geht nicht weil in component erst geladen werden muss
