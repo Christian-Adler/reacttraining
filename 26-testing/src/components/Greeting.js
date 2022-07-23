21482e8a215928a8f11dea19c2c77dab5d125948
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-23 18:17:15 +0200

    improved test

--- 26-testing/src/components/Greeting.js
+++ 26-testing/src/components/Greeting.js
@@ -2,9 +2,20 @@
 // import "./Greeting.css";
 // import classes from "./Greeting.module.css";
 
+import {useState} from "react";
+
 const Greeting = (props) => {
+  const [changedText, setChangedText] = useState();
+  
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+  
   return (<div><h2>Hello World!</h2>
-    <p>I's good to see you!</p></div>);
+    {!changedText && <p>I's good to see you!</p>}
+    {changedText && <p>Changed</p>}
+    <button onClick={changeTextHandler}>Change text</button>
+  </div>);
 };
 
 export default Greeting;
--- 26-testing/src/components/Greeting.test.js
+++ 26-testing/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 describe('Test suite: Greeting component', () => {
   
@@ -13,6 +14,44 @@ describe('Test suite: Greeting component', () => {
     // Assert
     const helloWorldElement = screen.getByText('Hello World', { exact: false }); // false, weil ! noch im DOM steht
     expect(helloWorldElement).toBeInTheDocument();
+  });
+  
+  test('Greeting: renders good to see you if the button was NOT clicked', () => {
+    // Arrange
+    render(<Greeting/>);
+    
+    // Act
+    // .. nothing - no functions calls here
+    
+    // Assert
+    const goodToSeeYouElement = screen.getByText('good to see you', { exact: false });
+    expect(goodToSeeYouElement).toBeInTheDocument();
+  });
+  
+  test('Greeting: Change text through button click', () => {
+    // Arrange
+    render(<Greeting/>);
+    
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+    
+    // Assert
+    const changedElement = screen.getByText('Changed', { exact: false });
+    expect(changedElement).toBeInTheDocument();
+  });
+  
+  test('Greeting: does not render "good to see you" after button click', () => {
+    // Arrange
+    render(<Greeting/>);
+    
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+    
+    // Assert
+    const changedElement = screen.queryByText('good to see you', { exact: false });
+    expect(changedElement).toBeNull();
   });
   
 });
