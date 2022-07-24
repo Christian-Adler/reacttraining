a722e16a6cf2bf0b7ceed795caa15f6e6d1d019c
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-24 21:10:40 +0200

    basics - primitives

---
+++ 27-1-typescript/basics.ts
@@ -0,0 +1,15 @@
+// Primitives: number, stirng, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number; // Number would be the Number object type - not the primitive
+age = 12; // ok
+// age = 'ab' // nok
+
+let userName: string;
+userName = 'chris';
+
+let isInstructor: boolean;
+isInstructor = false;
+
