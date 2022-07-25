befed7233090c4dac7d37637d1213733c6f30c26
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-07-25 21:29:28 +0200

    added description for generic props per component

--- 27-2-typescript-with-react/src/components/Todos.tsx
+++ 27-2-typescript-with-react/src/components/Todos.tsx
@@ -1,8 +1,25 @@
-function Todos() {
+import React from "react";
+
+// https://stackoverflow.com/questions/71788254/react-18-typescript-children-fc
+// type Props = {
+//     children?: React.ReactNode, // ? >> optional
+//     items: string[]
+// };
+
+// wenn children nicht gebraucht werden:
+// type Props = {
+//     items: string[]
+// };
+//
+// const Todos: React.FC<Props> = (props) => {
+
+// inline
+const Todos: React.FC<{ items: string[] }> = (props) => {
     return <ul>
-        <li>Learn React</li>
-        <li>Learn TS</li>
+        {
+            props.items.map(item => <li key={item}>{item}</li>)
+        }
     </ul>
-}
+};
 
 export default Todos;
